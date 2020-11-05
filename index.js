d91dd8ce78a77994b1f71a06697a66376dd4f21e
samiandreas70@gmail.com <samiandreas70@gmail.com>
2020-11-04 23:13:55 -0500

    generateMarkdown updtated with table of contents and data fields, array of questions added to index.js.

--- index.js
+++ index.js
@@ -1,4 +1,74 @@
-const fs= require("fs");
-const inquirer= require("inquirer");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const Choices = require("inquirer/lib/objects/choices");
+const generateMarkdown = ("./utils/generateMarkdown")
 
-console.log("Hello world");
+
+// array of questions for user
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "what is the project title?",
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What are steps to install the project?",
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "what is the project title?",
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "Provide instructions for the usage of this project",
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "who are the collaborators in this project?",
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "Select a License from the following options?",
+        Choices: [
+            "MIT",
+            "ISC",
+            "GPL"
+        ]
+    },
+];
+
+
+
+// function to write README file
+function writeToFile(README, data){
+        fs.writeFileSync(path.join(process.cwd(), README),data);
+    }
+
+
+    // console.log("New README file created with success!");
+
+
+
+// function to initialize program
+function init() {
+    inquirer.prompt(questions).then(answers=>{
+
+        //utilize the writeToFile method you created on line 50 - then give the file a name "ReadMe.md"- then utilize
+            //the generateMarkdown() 
+                //hint: the responses for the inquierer will be needed along with the spread operator
+
+
+   
+        // console.log("Working")
+    })
+
+}
+
+// function call to initialize program
+init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,44 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
-`;
-}
+  ${data.badge}
+
+  ## Description 
+  ${data.description}
+
+  ## Table of contents
+
+  - [Description](#Description)
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [Licence](#Licence)
+  - [Contributors](#Contributors)
+  - [Repository Link](#Repository)
+  - [Contact Info](#GitHub) 
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Licence
+  ${data.licence}
+
+  ## Contributors
+  ${data.contributing}
+
+  ## Test
+  ${data.test}
+  
+  ## Contact Info:
+
+  Github: [${data.Username}](https://github.com/Samuel-en)
+  Email: [${data.Email}] (samiandreas70@gmail.com)
+  
+  `;
+  }
 
 module.exports = generateMarkdown;
